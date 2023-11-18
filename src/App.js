84e335fd75bdf84d358ba1544021ecb792f4e45d
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2023-11-18 20:33:10 -0300

    Additional Libraries
    Added the Material UI library with the Icons ones and saved them in the package.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Btns from "./Btns";
 
 function App() {
-    return <div className="App">Hello world</div>;
+    return (
+        <div className="App">
+            <Btns />
+        </div>
+    );
 }
 
 export default App;
