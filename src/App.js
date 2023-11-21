9000063cd3923e328f8137d2ed936d1da678c1dc
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2023-11-20 21:55:23 -0300

    Container
    Added the container properties to the list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import TodoCard from "./TodoCard";
+import Container from "@mui/material/Container";
 
 function App() {
     return (
-        <div className="App">
-            <TodoCard />
-            <TodoCard />
+        <div
+            className="App"
+            style={{
+                height: "100vh",
+                backgroundColor: "#191B1E",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Container maxWidth="sm">
+                <TodoCard />
+            </Container>
         </div>
     );
 }
